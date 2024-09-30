6b5e122626a0baa957de997171412854878efef1
Subhash Bhalerao <csitstudentsguide@gmail.com>
2024-09-30 13:51:22 +0530

    Create Django app to Upload file with filetype check

--- test1/models.py
+++ test1/models.py
@@ -1,4 +1,16 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
+from django.core.exceptions import ValidationError
+import magic
+
+ext_validator = FileExtensionValidator(['pdf'])
+
+def validate_file_mimetype(file):
+    accept = ['application/pdf']
+    file_mime_type = magic.from_buffer(file.read(1024), mime=True)
+    print(file_mime_type)
+    if file_mime_type not in accept:
+        raise ValidationError('Unsupported file type')
 
 YEAR = [('I', 'I'), ('II', 'II'), ('III', 'III'), ('IV', 'IV')]
 
@@ -40,7 +52,7 @@ class ExpLists(models.Model):
     exp_no = models.CharField(max_length=50, choices=EXP_NO)
     ay = models.CharField(max_length=50, choices=AY)
     term = models.CharField(max_length=50, choices=TERM)
-    exp_file = models.FileField(upload_to='exp_lists/%Y/%m', blank=False)
+    exp_file = models.FileField(upload_to='exp_lists/%Y/%m', validators=[ext_validator], blank=False)
        
     
     def __str__(self):
--- test1/views.py
+++ test1/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import Test1Form
 
 # Create your views here.
-def test1_home(request):
-    form =  Test1Form()    
-    return render(request, 'test1/test1_home.html', {form:form})
+def test1_home(request):        
+    if request.method == 'POST':
+        form = Test1Form(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Done')
+    else:
+        form = Test1Form()
+
+    return render(request, 'test1/test1_home.html', {'form':form})
